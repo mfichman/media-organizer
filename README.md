ffa891ae8921da3aab0377bf527f351a5537b53a
Matt Fichman <matt.fichman@gmail.com>
2020-12-04 20:29:08 -0500

    Added README

---
+++ README.md
@@ -0,0 +1,10 @@
+
+# Media Organizer
+
+This is a tool that organizes photos using metadata. Usage:
+
+```
+pip install pipenv
+pipenv
+python -m organize
+```
--- organize.py
+++ organize.py
@@ -22,7 +22,20 @@ import metadata
 
 from models import Media, File
 
-def parse_digest(path):
+def file_size(path):
+    try:
+        print('stat:', path)
+        return path.stat().st_size
+    except:
+        return 0;
+
+def file_count_total(paths):
+    return sum(1 for _ in paths)
+
+def file_size_total(paths):
+    return sum(file_size(path) for path in paths)
+
+def digest_for(path):
     sha = hashlib.sha256()
     buf = bytearray(32768)
     mv = memoryview(buf)
@@ -33,10 +46,10 @@ def parse_digest(path):
 
     return sha.hexdigest()
 
-def parse_metadata(path):
+def metadata_for(path):
     return metadata.parse(path)
 
-def parse_path(args, digest, stem, taken_at, suffix):
+def path_for(args, digest, stem, taken_at, suffix):
     year = taken_at.strftime('%Y')
     month = taken_at.strftime('%m')
     day = taken_at.strftime('%d')
@@ -45,36 +58,39 @@ def parse_path(args, digest, stem, taken_at, suffix):
 
     return args.output.joinpath(year, month, day, stem).with_suffix(suffix)
 
-def parse_media(args, source):
+def record_for(args, source):
     suffix = source.suffix.lower()
 
     if suffix not in Media.types:
         return (source, None, ['skipped'])
 
-    digest = parse_digest(source)
-    raw, metadata = parse_metadata(source)
-    taken_at = metadata['taken_at']
+    try:
+        digest = digest_for(source)
+        raw, metadata = metadata_for(source)
+        taken_at = metadata['taken_at']
 
-    path = parse_path(args, digest, source.stem, taken_at, suffix)
+        path = path_for(args, digest, source.stem, taken_at, suffix)
 
-    stat = source.stat()
+        stat = source.stat()
 
-    result = dict(
-        name=str(source.name),
-        stem=str(source.stem),
-        extension=str(suffix),
-        metadata=json.dumps(raw),
-        path=str(path),
-        digest=str(digest),
-        size=int(stat.st_size),
-        taken_at=taken_at,
-        source=str(source),
-    )
+        result = dict(
+            name=str(source.name),
+            stem=str(source.stem),
+            extension=str(suffix),
+            metadata=json.dumps(raw),
+            path=str(path),
+            digest=str(digest),
+            size=int(stat.st_size),
+            taken_at=taken_at,
+            source=str(source),
+        )
 
-    messages = []
-    if not metadata: messages.append('failed to parse metadata')
+        messages = []
+        if not metadata: messages.append('failed to parse metadata')
 
-    return (source, result, messages)
+        return (source, result, messages)
+    except FileNotFoundError:
+        return (source, None, ['missing'])
 
 def save_media(params):
     query = Media.insert(params).on_conflict(conflict_target=[Media.source], update=params)
@@ -98,16 +114,11 @@ def input_iterator(args):
 def init_worker():
     signal.signal(signal.SIGINT, signal.SIG_IGN)
 
-def log_write(log, message):
-    log.write(message)
-    log.write('\n')
-    print(message)
-
-def find_files(args):
+def find(args):
     total = file_count_total(input_iterator(args))
 
     for index, path in enumerate(input_iterator(args)):
-        print('find {} {}/{}'.format(path, index, total))
+        print('find: {} {}/{}'.format(path, index, total))
         params = dict(path=path, extension=path.suffix, stem=path.stem)
         File.insert(params)\
             .on_conflict(
@@ -117,20 +128,14 @@ def find_files(args):
             )\
             .execute()
 
-def file_count_total(paths):
-    return sum(1 for _ in paths)
-
-def file_size_total(paths):
-    return sum(path.stat().st_size for path in paths)
-
-def read_files(args, pool, log):
+def process(args, pool, log):
     files = File.select().where(File.inspected_at.is_null()).execute()
     paths = [pathlib.Path(f.path) for f in files]
 
     count_total = file_count_total(paths)
     size_total = file_size_total(paths)
 
-    parse = functools.partial(parse_media, args)
+    parse = functools.partial(record_for, args)
 
     count_processed = 0
     size_processed = 0
@@ -138,10 +143,10 @@ def read_files(args, pool, log):
     for (path, result, messages) in pool.imap(parse, paths):
     #for (path, result, messages) in map(parse, input_iterator(args)):
         count_processed += 1
-        size_processed += path.stat().st_size
+        size_processed += file_size(path)
 
         for message in messages:
-            log_write(log, '{}: {}'.format(message, path))
+            print('error: {}: {}'.format(message, path))
 
         if result is None:
             mark_processed(path)
@@ -149,7 +154,7 @@ def read_files(args, pool, log):
 
         save_media(result)
 
-        print('read {} {} {}/{} {}/{}'.format(
+        print('process: {} {} {}/{} {}/{}'.format(
             result['source'],
             result['path'],
             count_processed,
@@ -158,7 +163,7 @@ def read_files(args, pool, log):
             humanize.naturalsize(size_total)
         ))
 
-def organize_files(args, pool, log):
+def organize(args, pool, log):
     photos = Media.select()\
         .where(Media.organized_at.is_null(), ~Media.duplicate)\
         .order_by(Media.id.asc())
@@ -172,11 +177,11 @@ def organize_files(args, pool, log):
         source = pathlib.Path(photo.source)
         try:
             source.rename(photo.path)
+            print('mv:', photo.source, photo.path)
         except FileNotFoundError as e:
-            log_write(log, str(e))
-        print('mv', photo.source, photo.path)
+            print('error:', str(e))
 
-def mark_duplicates():
+def deduplicate():
     gold = Media.select(
         peewee.fn.first_value(Media.id).over(
             partition_by=Media.digest,
@@ -201,27 +206,13 @@ def main():
     models.db.connect()
     models.db.create_tables([Media, File])
 
-    args.input = args.input or (
-        r'E:\JilliPhotos',
-        r'E:\iCloud Photos',
-        #r'E:\Google Photos (Newly Added Photos)',
-        #r'E:\Google Drive\Photos\Natalie',
-        #r'E:\Google Drive\Videos\Family Videos',
-        #r'E:\Google Drive\Videos\Year',
-    )
-
-    #import logging
-    #logger = logging.getLogger('peewee')
-    #logger.addHandler(logging.StreamHandler())
-    #logger.setLevel(logging.DEBUG)
-
     pool = multiprocessing.Pool(2, init_worker)
     log = open('media.log', 'w')
 
-    find_files(args)
-    read_files(args, pool, log)
-    mark_duplicates()
-    organize_files(args, pool, log)
+    find(args)
+    process(args, pool, log)
+    deduplicate()
+    organize(args, pool, log)
 
 if __name__ == '__main__':
     try:
